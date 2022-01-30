26e188bf549e0010fa14fe6a965b1cad0a777c2a
Chris <chrismann2001@gmail.com>
2022-01-30 12:10:09 -0800

    part4

---
+++ Lab/Lab1/part4/lab1p4_p2.c
@@ -0,0 +1,79 @@
+#include "stdio.h"
+#include "string.h"
+#include <stdlib.h>
+
+
+//starts from end of string and removes all special characters until alphanumeric ex: file!! -> goes to file, fi!le -> fi!le
+void remove_end_special_characters(char *string){
+   for(int i = strlen(string)-1; i>=0; i--){
+      if ( ((string[i]>= '0') && (string[i]<= '9') ) || 
+         ( (string[i] >= 'a' )&& (string[i] <= 'z')) ||         
+         ( (string[i] >= 'A') && (string[i] <= 'Z') )){
+         break;
+      }
+      
+      else{
+         string[i] = '\0'; //remove character
+      }
+      
+   }
+}
+
+//main design function
+void file_word_count(const char *file_name){
+	FILE *input = fopen(file_name, "r"); //open input text
+	char input_txt[500]; //string for holding each word
+	char *distinct_words[500]; //array of strings for distinct words
+	int  counter = 0; //counts number of distinct words
+	
+	//if input file doesn't open print error
+	if(input == NULL){
+		printf("Could not open file: %s",file_name);
+	}
+	
+	
+	else{
+	
+	
+	   //reading input file
+		while(fscanf(input,"%s",input_txt)!= EOF){
+		
+		   //remove special character punctation from end of word
+		   remove_end_special_characters(input_txt);
+		   
+		   int flag = 0; //flag if for repeated word
+			for(int i = 0; i<counter;i++){
+				if(strcasecmp(input_txt,distinct_words[i]) == 0){ //if repeated word
+				      flag = 1;
+			      	break;
+				}				
+			}
+			if(flag == 0){
+		      distinct_words[counter] = (char *) malloc(sizeof(input_txt)); //allocates memory for new distinct work
+		      strcpy(distinct_words[counter],input_txt); //copies distinct word
+		      counter++;
+		   }
+ 
+		}		
+		fclose(input);
+		
+		
+		//writing output file
+		FILE *output = fopen("out_improved.txt", "w");
+		fprintf(output,"Number of distinct words: %d \n",counter);
+		fprintf(output,"List of distinct words: ");
+		for(int i = 0; i < counter; i++){
+		   fprintf(output, "%s ",distinct_words[i]);
+		}
+		fclose(output);
+	}
+	
+	return;
+
+}
+
+int main(){
+
+	file_word_count("words.txt");
+
+}
